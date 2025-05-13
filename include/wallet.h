dfd20b43c2ddbcac8b92e4e90371ba469550c142
darknguyen09 <42785567+darknguyen09@users.noreply.github.com>
2025-05-13 17:33:22 +0700

    Update wallet.h

--- include/wallet.h
+++ include/wallet.h
@@ -6,19 +6,20 @@
 
 class User;
 
+// luu tru thong tin vi diem thuong
 class Wallet {
 public:
-    std::string walletID;
-    double balance;
-    std::vector<std::string> transactionLog;
+    std::string walletID; // id vi
+    double balance; // so du vi
+    std::vector<std::string> transactionLog; // lich su giao dich
 
     Wallet(std::string id, double b = 0.0);
 };
 
-void saveWallets(const std::vector<Wallet>& wallets, const std::string& relativeFilename);
-std::vector<Wallet> loadWallets(const std::string& relativeFilename);
+void saveWallets(const std::vector<Wallet>& wallets, const std::string& relativeFilename); // luu danh sach vi
+std::vector<Wallet> loadWallets(const std::string& relativeFilename); // tai danh sach vi
 
-void transferPoints(User& sender, std::vector<User>& users, std::vector<Wallet>& wallets);
-void viewWallet(const User& user, const std::vector<Wallet>& wallets);
+void transferPoints(User& sender, std::vector<User>& users, std::vector<Wallet>& wallets); // chuyen diem
+void viewWallet(const User& user, const std::vector<Wallet>& wallets); // xem vi
 
 #endif
